ab9968986fab1c20aaa9670e521b5d4a33683ea5
AAPL00 <aapldev00@proton.me>
2026-07-28 11:58:10 -0400

    chore: remove unused dashboard route and dead Breeze layout files

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', fn () => redirect()->route('profile.edit'))
-    ->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
